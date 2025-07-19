2b87064e4c3dac36c2328b2203830711433946d2
Kalash Sharma <kalash.strt@gmail.com>
2025-07-19 18:33:05 +0530

    chore(setup): new springboot application

---
+++ src/main/java/com/k99sharma/inferno/InfernoApplication.java
@@ -0,0 +1,13 @@
+package com.k99sharma.inferno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfernoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfernoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k99sharma/inferno/InfernoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k99sharma.inferno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfernoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
