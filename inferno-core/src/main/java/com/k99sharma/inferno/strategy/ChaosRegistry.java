19971392c7a77a0cdb6132b1e9058f422f594e7c
Kalash Sharma <kalash.strt@gmail.com>
2025-07-20 18:03:44 +0530

    fix(chaos registry): modified chaosMap to ConcurrentHashMap to make it thread safe

--- inferno-core/src/main/java/com/k99sharma/inferno/strategy/ChaosRegistry.java
+++ inferno-core/src/main/java/com/k99sharma/inferno/strategy/ChaosRegistry.java
@@ -2,11 +2,11 @@ package com.k99sharma.inferno.strategy;
 
 import com.k99sharma.inferno.model.FailureMode;
 
-import java.util.EnumMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ChaosRegistry {
-    private final Map<FailureMode, ChaosStrategy> chaosMap = new EnumMap<>(FailureMode.class);
+    private final Map<FailureMode, ChaosStrategy> chaosMap = new ConcurrentHashMap<>();
 
     public void register(FailureMode mode, ChaosStrategy strategy) {
         chaosMap.put(mode, strategy);
@@ -18,5 +18,9 @@ public class ChaosRegistry {
 
     public ChaosStrategy get(FailureMode mode) {
         return chaosMap.get(mode);
+    }
+
+    public boolean contains(FailureMode mode) {
+        return chaosMap.containsKey(mode);
     }
 }
